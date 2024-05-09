6bfcb144923e2898233acd2610c5188f5b321812
JobCollins <dulo.collins@gmail.com>
2024-05-09 17:01:56 +0300

    Feature: Geolocation
    input address of a place and get back longitude and latitude
    get soil information based on coordinates

---
+++ climate_functions.py
@@ -0,0 +1,63 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+def convert_to_mm_per_month(monthly_precip_kg_m2_s1):
+    days_in_months = np.array([31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31])
+    return monthly_precip_kg_m2_s1 * 60 * 60 * 24 * days_in_months
+
+@st.cache_data
+def extract_climate_data(lat, lon, _hist, _future):
+    """
+    Extracts climate data for a given latitude and longitude from historical and future datasets.
+
+    Args:
+    - lat (float): Latitude of the location to extract data for.
+    - lon (float): Longitude of the location to extract data for.
+    - hist (xarray.Dataset): Historical climate dataset.
+    - future (xarray.Dataset): Future climate dataset.
+
+    Returns:
+    - df (pandas.DataFrame): DataFrame containing present day and future temperature, precipitation, and wind speed data for each month of the year.
+    - data_dict (dict): Dictionary containing string representations of the extracted climate data.
+    """
+    hist_temp = _hist.sel(lat=lat, lon=lon, method="nearest")["tas"].values - 273.15
+    hist_temp_str = np.array2string(hist_temp.ravel(), precision=3, max_line_width=100)[
+        1:-1
+    ]
+
+    hist_pr = _hist.sel(lat=lat, lon=lon, method="nearest")["pr"].values
+    hist_pr = convert_to_mm_per_month(hist_pr)
+
+    hist_pr_str = np.array2string(hist_pr.ravel(), precision=3, max_line_width=100)[
+        1:-1
+    ]
+
+
+    future_temp = _future.sel(lat=lat, lon=lon, method="nearest")["tas"].values - 273.15
+    future_temp_str = np.array2string(
+        future_temp.ravel(), precision=3, max_line_width=100
+    )[1:-1]
+
+    future_pr = _future.sel(lat=lat, lon=lon, method="nearest")["pr"].values
+    future_pr = convert_to_mm_per_month(future_pr)
+    future_pr_str = np.array2string(future_pr.ravel(), precision=3, max_line_width=100)[
+        1:-1
+    ]
+
+    df = pd.DataFrame(
+        {
+            "Present Day Temperature": hist_temp[0, 0, :],
+            "Future Temperature": future_temp[0, 0, :],
+            "Present Day Precipitation": hist_pr[0, 0, :],
+            "Future Precipitation": future_pr[0, 0, :],
+            "Month": range(1, 13),
+        }
+    )
+    data_dict = {
+        "hist_temp": hist_temp_str,
+        "hist_pr": hist_pr_str,
+        "future_temp": future_temp_str,
+        "future_pr": future_pr_str,
+    }
+    return df, data_dict

3984361dac8efc19dc3abfe5a0c73e2c1ff2e929
JobCollins <dulo.collins@gmail.com>
2024-05-21 13:20:53 +0300

    Bug: Llama3 did not respond with valid json

--- src/cds_api_call.py
+++ src/cds_api_call.py
@@ -97,6 +97,7 @@ def load_hist_proj(data_dir):
     proj_data = xr.open_mfdataset(f'{data_dir}*CanESM2_rcp45*.nc')
     return hist_data, proj_data
 
+@st.cache_data
 def load_seasonal_forecast(data_dir):
     seas5_forecast = xr.open_dataset(f'{data_dir}/seasonal/ecmwf_seas5_2024_03_forecast_monthly_tp.grib', engine='cfgrib', 
                                  backend_kwargs=dict(time_dims=('forecastMonth', 'time')))
